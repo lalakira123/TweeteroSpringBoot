b5166e84e1dd9cab86a6214b8f2ef483e83044bb
Laerte <laerteakira@gmail.com>
2023-01-23 19:46:49 -0300

    feat: GET Method /tweets

--- src/main/java/com/twettero/api/controller/TweetController.java
+++ src/main/java/com/twettero/api/controller/TweetController.java
@@ -1,12 +1,20 @@
 package com.twettero.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.twettero.api.dto.TweetDTO;
+import com.twettero.api.model.Tweet;
 import com.twettero.api.service.TweetService;
 
 import jakarta.validation.Valid;
@@ -17,6 +25,12 @@ public class TweetController {
   
   @Autowired
   private TweetService service;
+
+  @GetMapping
+  public Page<Tweet> list(Pageable page) {
+    PageRequest pr = PageRequest.of(0, 5);
+    return service.findAll(pr);
+  }
 
   @PostMapping
   public void create(@RequestBody @Valid TweetDTO req) {
--- src/main/java/com/twettero/api/service/TweetService.java
+++ src/main/java/com/twettero/api/service/TweetService.java
@@ -1,6 +1,11 @@
 package com.twettero.api.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import com.twettero.api.dto.TweetDTO;
@@ -12,6 +17,10 @@ public class TweetService {
   
   @Autowired
   private TweetRepository repository;
+
+  public Page<Tweet> findAll(PageRequest page) {
+    return repository.findAll(page);
+  }
 
   public Tweet save(TweetDTO dto) {
     return repository.save(new Tweet(dto));
